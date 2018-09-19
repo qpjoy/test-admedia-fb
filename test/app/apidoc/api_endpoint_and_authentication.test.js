d8e028d4df59837f0fd29ce5538fcc5755da9f63
qpjoy2018 <qpjoy2018@163.com>
2018-09-19 18:19:26 +0800

    init: test-fb-api

---
+++ test/app/apidoc/api_endpoint_and_authentication.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { app, assert } = require('egg-mock/bootstrap');
+
+
+describe('Basic or Core Targeting', () => {
+
+  // it('should assert', function* () {
+  //   const pkg = require('../../../package.json');
+  //   assert(app.config.keys.startsWith(pkg.name));
+  //
+  //   // const ctx = app.mockContext({});
+  //   // yield ctx.service.xx();
+  // });
+  //
+  // it('basic core targeting should GET /', () => {
+  //   return app.httpRequest()
+  //     .get('/')
+  //     .expect('hi, egg')
+  //     .expect(200);
+  // });
+  //
+  // it('baidu should response', async () => {
+  //   await app.httpRequest()
+  //     .get('http://www.baidu.com')
+  //     .expect(200);
+  // });
+});
